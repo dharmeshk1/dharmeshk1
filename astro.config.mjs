a94dc8aa23535ea5b7f2f6b07562ab88e8ca9ef1
dharmeshk1 <105537447+dharmeshk1@users.noreply.github.com>
2026-09-18 11:25:30 +0530

    Configure GitHub Pages project base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://dharmeshk1.github.io',
-  base: '/',
+  site: 'https://dharmeshk1.github.io/dharmeshk1',
+  base: '/dharmeshk1/',
   integrations: [sitemap()]
 });
